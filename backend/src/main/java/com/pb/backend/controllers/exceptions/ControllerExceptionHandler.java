f5d1e2caca034a5bbabf5edde494d704adee9b60
pcechetto <pedrocechetto@hotmail.com>
2025-07-14 14:11:50 -0300

    app & mongo container, ControllerExceptionHandler

---
+++ backend/src/main/java/com/pb/backend/controllers/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.pb.backend.controllers.exceptions;
+
+import com.pb.backend.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError error = new StandardError();
+        error.setTimestamp(Instant.now());
+        error.setStatus(status.value());
+        error.setError("Entity not found");
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+}
